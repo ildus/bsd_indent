8217f140f87f5c23d67db14c7b78bf78b59ef8bb
Ildus K <i.kurbangaliev@gmail.com>
2015-10-19 17:12:21 +0300

    Add strlcpy function

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ PROG=	indent
 SRCS=	indent.c io.c lexi.c parse.c pr_comment.c args.c reallocarray.c
 
 all:
-	${CC} ${SRCS} -o ${PROG}
+	${CC} ${SRCS} -o ${PROG} ${CCFLAGS}
 check:
 	cat test.in | ./indent -bc -bl -nce > _test.out
 	diff test.out _test.out
--- lexi.c
+++ lexi.c
@@ -111,8 +111,7 @@ char        chartype[128] =
     1, 1, 1, 0, 3, 0, 3, 0
 };
 
-
-
+size_t strlcpy(char *dst, const char *src, size_t siz);
 
 int
 lexi(void)
@@ -602,4 +601,38 @@ addkey(char *key, int val)
     p->rwcode = val;
     nspecials++;
     return;
+}
+
+/*
+ * Copy src to string dst of size siz.  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ * Function creation history:  http://www.gratisoft.us/todd/papers/strlcpy.html
+ */
+size_t strlcpy(char *dst, const char *src, size_t siz)
+{
+	char	   *d = dst;
+	const char *s = src;
+	size_t		n = siz;
+
+	/* Copy as many bytes as will fit */
+	if (n != 0)
+	{
+		while (--n != 0)
+		{
+			if ((*d++ = *s++) == '\0')
+				break;
+		}
+	}
+
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (n == 0)
+	{
+		if (siz != 0)
+			*d = '\0';			/* NUL-terminate dst */
+		while (*s++)
+			;
+	}
+
+	return (s - src - 1);		/* count does not include NUL */
 }
